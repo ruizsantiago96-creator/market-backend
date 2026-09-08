3a36046af8efb3a5b3d9652ab9d4c7d86020f247
Lourdes Ruiz <ruizsantiago96@gmail.com>
2026-09-08 01:04:21 -0600

    inicio commit

---
+++ src/test/java/com/tecnm/merida/market_backend/MarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnm.merida.market_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
